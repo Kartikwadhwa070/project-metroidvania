2ddd371490b510392fe3a5921aa845d96d4a1d33
Kartikwadhwa070 <168586991+Kartikwadhwa070@users.noreply.github.com>
2024-11-23 19:26:39 +0530

    added melee combat
    ~added attack animation
    ~added attack area detection
    ~added attack animation
    ~added damage to enemy class
    ~added slash effect animation to attacks

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -6,7 +6,7 @@ public class playerController : MonoBehaviour
 {
     [Header("Horizontal Movement Settings")]
     private Rigidbody2D rb;
-    private float xAxis;
+    private float xAxis, yAxis;
     private float gravity;
     [Header("Ground Check Settings")]
     [SerializeField] private float walkSpeed = 1;
@@ -35,9 +35,19 @@ public class playerController : MonoBehaviour
     private bool canDash = true;
     [SerializeField] private float dashSpeed;
     [SerializeField] private float dashTime;
-    [SerializeField] private float dashCooldown;   
+    [SerializeField] private float dashCooldown;
     private bool dashed;
     [SerializeField] GameObject dashEffect;
+
+    [Header("Attacking")]
+    bool attack = false;
+    float timeBetweenAttack, timeSinceAttack;
+    [SerializeField] Transform SideAttackTransform, UpAttackTransform, DownAttackTransform;
+    [SerializeField] Vector2 SideAttackArea, UpAttackArea, DownAttackArea;
+    [SerializeField] LayerMask EnemyLayer;
+    [SerializeField] float damage;
+    [SerializeField] GameObject slashEffect; 
+
 
     // Start is called before the first frame update
     private void Awake()
@@ -59,6 +69,14 @@ public class playerController : MonoBehaviour
         gravity = rb.gravityScale;
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(SideAttackTransform.position, SideAttackArea);
+        Gizmos.DrawWireCube(DownAttackTransform.position, DownAttackArea);
+        Gizmos.DrawWireCube(UpAttackTransform.position, UpAttackArea);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -69,10 +87,13 @@ public class playerController : MonoBehaviour
         Move();
         Jump();
         StartDash();
+        Attack();
     }
     void GetInputs()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+        attack = Input.GetMouseButtonDown(0);
     }
     void flip()
     {
@@ -101,7 +122,7 @@ public class playerController : MonoBehaviour
     }
     IEnumerator Dash()
     {
-        canDash = false;  
+        canDash = false;
         if (!Grounded() && hasAirDashed)
         {
             canDash = true;
@@ -121,6 +142,53 @@ public class playerController : MonoBehaviour
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
+
+    void Attack()
+    {
+        timeSinceAttack += Time.deltaTime;
+        if (attack && timeSinceAttack >= timeBetweenAttack)
+        {
+            timeSinceAttack = 0;
+            anim.SetTrigger("Attacking");
+
+            if (yAxis == 0 || yAxis < 0 && Grounded())
+            {
+                Hit(SideAttackTransform, SideAttackArea);
+                Instantiate(slashEffect, SideAttackTransform);
+            }
+            else if (yAxis > 0)
+            {
+                Hit(UpAttackTransform, UpAttackArea);
+                slashEffectAtAngle(slashEffect, 90, UpAttackTransform);
+            }
+            else if (yAxis < 0 && !Grounded())
+            {
+                Hit(DownAttackTransform, DownAttackArea);
+                slashEffectAtAngle(slashEffect, -90, DownAttackTransform);
+            }
+        }
+    }
+
+    void Hit(Transform _attackTransform, Vector2 _attackArea)
+    {
+        Collider2D[] objectsToHit = Physics2D.OverlapBoxAll(_attackTransform.position, _attackArea, 0, EnemyLayer);
+        for (int i = 0; i < objectsToHit.Length; i++)
+        {
+            Enemy enemy = objectsToHit[i].GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.EnemyHit(damage);
+            }
+        }
+    }
+
+    void slashEffectAtAngle(GameObject _slashEffect, int _effectAngle, Transform _attackTransform)
+    {
+        _slashEffect = Instantiate(_slashEffect, _attackTransform);
+        _slashEffect.transform.eulerAngles = new Vector3(0, 0, _effectAngle);
+        _slashEffect.transform.localScale = new Vector2(transform.localScale.x,_attackTransform.localScale.y); 
+    }
+
     public bool Grounded()
     {
         if (Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckY, whatIsGround) || Physics2D.Raycast(groundCheck.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) || Physics2D.Raycast(groundCheck.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
